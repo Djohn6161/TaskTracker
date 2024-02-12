8623fcc4442ab6044a4381da7c64a187b3724766
Don John Daryl P. Curativo <doncurativo@my.cspc.edu.ph>
2024-02-12 14:52:03 +0800

    props react

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
+import Header from './components/Header'
 function App() {
   const name = 'Don John Daryl Curativo';
   const x = false;
 
   return (
     <div className="container">
-      <h1>Hello World my Name is {name} And I'm learning React</h1>
-      <h2>I'm {x ? 'male' : 'female'}</h2>
+      <Header ></Header>
     </div>
   );
 }
-
 export default App;
