c6aa9cdbe892bb3c47457dc3d6d71ff5e7ac360e
Don John Daryl P. Curativo <doncurativo@my.cspc.edu.ph>
2024-02-12 18:48:40 +0800

    tas

--- src/App.js
+++ src/App.js
@@ -37,12 +37,24 @@ function App() {
     },
   ]);
   const deleteTask = (id) => {
-    setTasks(tasks.filter((task) => task.id !== id))
-  }
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+  const toggleReminder = (id) => {
+    // console.log(id);
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
   return (
     <div className="container">
       <Header></Header>
-      {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} />) : ('No Tasks to Show')}
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        "No Tasks to Show"
+      )}
     </div>
   );
 }
