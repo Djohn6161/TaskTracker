c86e36bca9fb2aac55c5a814c01444fff419a993
Don John Daryl P. Curativo <doncurativo@my.cspc.edu.ph>
2024-02-12 14:58:18 +0800

    CSS in JS

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,23 @@
+import PropTypes from 'prop-types'
 
-const Header = (props) => {
+const Header = ({title}) => {
   return (
     <header>
-        <h1> {props.title}</h1>
+        <h1 style={headingStyle}> {title}</h1>
     </header>
   )
 }
 
 Header.defaultProps = {
     title: 'Task Tracker',
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
+}
+
+const headingStyle = {
+    color: 'red',
+    backgroundColor: 'black'
 }
 export default Header
