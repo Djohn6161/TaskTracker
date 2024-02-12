ae46ff125f4a68818ae2ec114575b5a20e4e909d
Don John Daryl P. Curativo <doncurativo@my.cspc.edu.ph>
2024-02-12 14:45:47 +0800

    hello world

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  const name = 'Don John Daryl Curativo';
+  const x = false;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Hello World my Name is {name} And I'm learning React</h1>
+      <h2>I'm {x ? 'male' : 'female'}</h2>
     </div>
   );
 }
