3906f0e185a1b0d672191d85d0310ee14f45205c
Don John Daryl P. Curativo <doncurativo@my.cspc.edu.ph>
2024-02-12 16:39:34 +0800

    delete tasks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
-import Header from './components/Header'
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import { useState } from "react";
 function App() {
-  const name = 'Don John Daryl Curativo';
+  const name = "Don John Daryl Curativo";
   const x = false;
-
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Doctors Appointments",
+      day: "February 14 at 2:00PM",
+      reminder: "true",
+    },
+    {
+      id: 2,
+      text: "Fated Day",
+      day: "February 18 at 1:00PM",
+      reminder: "true",
+    },
+    {
+      id: 3,
+      text: "Teeth Checkup",
+      day: "February 29 at 1:00PM",
+      reminder: "true",
+    },
+    {
+      id: 4,
+      text: "Party!",
+      day: "February 20 at 1:00AM",
+      reminder: "true",
+    },
+    {
+      id: 5,
+      text: "Tambay",
+      day: "February 19 at 6:00AM",
+      reminder: "true",
+    },
+  ]);
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
   return (
     <div className="container">
-      <Header ></Header>
+      <Header></Header>
+      {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} />) : ('No Tasks to Show')}
     </div>
   );
 }
